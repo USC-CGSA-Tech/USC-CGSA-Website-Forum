7fac7eec9603e58efb64a1ce09ad62a96b8e6847
flyingbearmt <tongmiao@usc.edu>
2019-02-26 23:08:08 -0800

    forum 1.2
    complish the link to the  google form

---
+++ USCCGSA Website/forum/forum.js
@@ -0,0 +1,10 @@
+
+
+document.querySelector(".socialQRcode").onmouseenter= function(){
+			
+			document.querySelector("#wechatqrcode").classList.remove("hide");
+		}
+document.querySelector(".socialQRcode").onmouseleave= function(){
+			
+			document.querySelector("#wechatqrcode").classList.add("hide");
+		}
